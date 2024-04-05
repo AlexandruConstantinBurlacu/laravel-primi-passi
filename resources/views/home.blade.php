f8c9c1924bd33fff74dbfd05072a5ff16af2431d
Alex Burlacu <alexandruburlacu03042000@gmail.com>
2024-04-05 15:54:31 +0200

    inserimento e visualizzazione home.blade.php

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>laravel-primi-passi</title>
+</head>
+<body>
+    <h1>
+        Laravel primi passi
+    </h1>
+</body>
+</html>
